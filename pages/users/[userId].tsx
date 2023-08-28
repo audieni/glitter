00586bfabf613c3e847ba454529a9afb0a64aefd
tasukaru <111254997+tasukaru@users.noreply.github.com>
2023-08-28 12:34:54 -0400

    added bio portion of user profile

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -3,6 +3,7 @@ import {useRouter} from "next/router";
 import useUser from "@/hooks/useUser";
 import {ClipLoader} from "react-spinners";
 import UserHero from "@/components/users/UserHero";
+import UserBio from "@/components/users/UserBio";
 
 const UserView = () => {
     const router = useRouter();
@@ -27,6 +28,7 @@ const UserView = () => {
         <>
             <Header showBackArrow label={fetchedUser?.name} />
             <UserHero userId={userId as string} />
+            <UserBio userId={userId as string} />
         </>
     )
 }
