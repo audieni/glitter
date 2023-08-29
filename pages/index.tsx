e123835a7ffbaa82db7da75ed02ecf2f842cd5b4
tasukaru <111254997+tasukaru@users.noreply.github.com>
2023-08-29 14:10:17 -0400

    added post feed to home and user profiles

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,15 @@ import {Inter} from 'next/font/google'
 import Header from "@/components/Header";
 import {useRouter} from "next/router";
 import {useCallback} from "react";
+import Form from "@/components/Form";
+import PostFeed from "@/components/posts/PostFeed";
 
 export default function Home() {
     return (
-        <Header label={"Home"} />
+        <>
+            <Header label={"Home"}/>
+            <Form placeholder={"What's happening?"}/>
+            <PostFeed />
+        </>
     );
 }
